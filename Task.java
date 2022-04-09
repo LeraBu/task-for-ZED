2d097ca28316eee61d14b490920b91be4443b377
Lera Burtseva <burtsevaleria@gmail.com>
2022-04-09 12:38:23 +0300

    decision start

---
+++ Task.java
@@ -0,0 +1,10 @@
+public class Task {
+    public static void main(String[] args) {
+        String route = "NSEWWSENNE";
+        System.out.println(isItAGoodRoute(route));
+    }
+
+    public static String isItAGoodRoute(String route){
+        return "Это плохой маршрут";
+    }
+}
