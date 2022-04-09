dbfa8f4cb32d32b047dfc9a06cbba86debb2ce20
Lera Burtseva <burtsevaleria@gmail.com>
2022-04-09 14:08:21 +0300

    decision one

--- Task.java
+++ Task.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Task {
     public static void main(String[] args) {
         String route = "NSEWWSENNE";
@@ -5,6 +7,18 @@ public class Task {
     }
 
     public static String isItAGoodRoute(String route){
-        return "Это плохой маршрут";
+        int x = 0;
+        int y = 0;
+        char[] result = route.toCharArray();
+        for (int i = 0; i<result.length; i++){
+            if (result[i] == 'N') x++;
+            else if (result[i] == 'S') x--;
+            else if (result[i] == 'E') y++;
+            else if (result[i] == 'W') y--;
+            else System.out.println("В построении маршрута ошибка");
+        }
+        if ((x == 0) && (y == 0))
+        return "Это хороший маршрут";
+        else return "Это плохой маршрут";
     }
 }
